b34d2544a4e8da5dbced5c4bca9c2372c63668e3
JLLx86 <eabduljalil@outlook.com>
2020-02-09 23:00:32 +0100

    Declares the global variable mfwidth_found becuase it was used in assignment without a declaration

--- src/libftprintf.h
+++ src/libftprintf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhaddi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: jll32 <jll32@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 11:00:58 by mhaddi            #+#    #+#             */
-/*   Updated: 2020/01/23 17:12:16 by mhaddi           ###   ########.fr       */
+/*   Updated: 2020/02/09 22:55:32 by jll32            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ int minusflag_found;
 int zeroflag_found;
 int precisiondot_found;
 int precision_val;
-int mfwidth_val;	// about "mfwidth_found": it's not a thing
-			// because if mfwidth_val > 0 : it's found
-			// but if it was = 0, it's considered as
-			// not found since 0 is default anyway:
-			// if we justified by 0 nothing will happen
+int mfwidth_found;
+int mfwidth_val; // about "mfwidth_found": it's not a thing
+				 // because if mfwidth_val > 0 : it's found
+				 // but if it was = 0, it's considered as
+				 // not found since 0 is default anyway:
+				 // if we justified by 0 nothing will happen
 va_list ap;
 char specifier;
 int counter;
