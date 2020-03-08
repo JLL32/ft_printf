86608f2309278a7ff27fa9bb8e267dece5b7def3
JLLx86 <eabduljalil@outlook.com>
2020-03-08 22:03:00 +0100

    Adds a minimal testing structure

--- src/main.c
+++ src/main.c
@@ -6,12 +6,31 @@
 #include <stdbool.h>
 #include "libftprintf.h"
 
-int	main()
+enum tests
 {
-	//puts("Mine-------\n");
-	ft_printf("lmao %05i, %-7i, %i, %d|\n", 5, 9, 10, 20);
-	printf("lmao %05i, %-7i, %i, %d|\n", 5, 9, 10, 20);
-	//puts("-----------\n");
-	//printf("lmao %-20d %20d\n", 5, 9);
+	decimal_with_flag_zero = 1,
+	decimal_with_flag_minus,
+	decimal_with_flag_minuszero,
+
+};
+void test(int tests);
+
+int main()
+{
+	test(decimal_with_flag_zero);
 	return 0;
+}
+
+void	test(tests)
+{
+	switch (tests)
+	{
+	case decimal_with_flag_zero:
+		ft_printf("lmao %05i, %010i, %i, %d|\n", 5, 9, 10, 20);
+		printf("lmao %05i, %010i, %i, %d|\n", 5, 9, 10, 20);
+		break;
+
+	default:
+		break;
+	}
 }
