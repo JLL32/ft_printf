2c4c4e8d3732f436977bc4dbf8aa26ef4568e5d7
JLLx86 <eabduljalil@outlook.com>
2020-10-29 19:03:08 +0100

    just another removal

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jll32 <jll32@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 08:35:53 by ael-hach          #+#    #+#             */
-/*   Updated: 2020/10/29 18:00:35 by jll32            ###   ########.fr       */
+/*   Updated: 2020/10/29 18:59:50 by jll32            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,6 @@ void	init_form(void)
 	g_form.flags.minus = false;
 	g_form.width = 0;
 	g_form.precision = -1;
-	g_form.length = '\0';
 	g_form.specifier = '\0';
 	return ;
 }
--- src/libftprintf.h
+++ src/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jll32 <jll32@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 10:46:25 by ael-hach          #+#    #+#             */
-/*   Updated: 2020/10/29 18:29:55 by jll32            ###   ########.fr       */
+/*   Updated: 2020/10/29 19:02:10 by jll32            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ void			get_width(void);
 void			get_precision(void);
 void			get_specifier(void);
 void			parse_signed(long arg
-, int field_width
-, size_t padding
-, size_t len);
+				, int field_width
+				, size_t padding
+				, size_t len);
 void			parse_ptr(size_t arg);
 void			parse_str(char *arg);
 void			parse_char(int arg);
@@ -55,7 +55,6 @@ typedef struct
 	}		flags;
 	int		width;
 	int		precision;
-	char	length;
 	char	specifier;
 }		t_form;
 
